772898c3d743143438ecb1567327a08e090b59da
alexc <alexc@SONY-VAIO>
2023-03-21 22:46:59 +0400

    Add project files.

---
+++ Lesson 6/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Lesson_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int t = Convert.ToInt32(Console.ReadLine());
+            Ref(ref t);
+            Out(out t);
+        }
+
+        public static int Ref(ref int t)
+        {
+            t = t * t;
+            return 0;
+        }
+        public static int Out(out int t)
+        {
+            t = ;
+            return 0;
+        }
+    }
+}
